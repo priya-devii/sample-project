907e4597c3a1ce0d511c44b11a8fbb02e78ebcd1
priya-devii <priyadevithedeveloper@gmail.com>
2019-04-17 13:51:53 +0530

    second add project

---
+++ My_bubble/bubble.js
@@ -0,0 +1,33 @@
+
+
+
+
+document.onmousemove = animatecircles;
+
+var colors = ['#85faff','99fbff','#acfcff','#c0fcff','#d3fdff','#85faff'];
+
+
+
+function animatecircles (event){
+    var circle =document.createElement("div");
+    circle.setAttribute("class","circle");
+    document.body.appendChild(circle);
+
+    circle.style.left = event.clientX + 'px';
+    circle.style.top = event.clientY + 'px';
+
+   var color = colors[Math.floor(Math.random() * colors.length)];
+   circle.style.backgroundColor = color;
+   circle.style.borderColor = color;
+
+    circle.style.transition="all 0.5s linear 0s";
+
+    circle.style.left = circle.offsetLeft - 20 + 'px';
+    circle.style.top = circle.offsetTop - 20 + 'px';
+
+    circle.style.width= "50px";
+    circle.style.height ="50px" ;
+    circle.style.borderWidth = "5px";
+    circle.style.opacity= 0;
+
+}
